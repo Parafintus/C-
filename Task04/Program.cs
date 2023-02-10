45c23e695993e3d40f9ef39eb898760ad2f38110
Nikita <ba999@mail.ru>
2023-02-10 23:39:17 +0300

    added conditions

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите первое число");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
